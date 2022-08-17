6d4b5aeb34f0d35cf867572077c366151cafef59
alianjum <m.alianjum0@gmail.com>
2022-08-17 14:26:36 +0200

    Added lamda functionality and slack notification

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Simple bot to like a user's tweets
+# Simple bot to like a friend tweets
 
+Bot to automatically like your friends' tweets, and notify you on your Slack channel.
+
+### Practical use cases
+
+ - You are like me. You don't have time to check social media and you want to give attention to someone.
+ - You are in a relationship. Your girlfriend is constantly nagging you for not being the `first-one` to like her tweets.
+
+How does it work?
+================  
+
+ This script runs tweeter API and checks for any new tweets for a paticular `user_id` in last 15 minutes. If a new a tweet is found it likes the tweet and sends a notification to your configured Slack channel using Slack Webhooks.
+
+Installation
+===============
+
+ - `git clone https://github.com/alianjum0/tweet-like-bot`
+ - `npm install`
+ - create a `.env` file (you must set `USER_ID` (Self user id), `FRIEND_USER_ID` (Friend user id) from [TweeterId](https://tweeterid.com),`CONSUMER_KEY`,`CONSUMER_SECRET`, `ACCESS_TOKEN, `ACCESS_SECRET`, `CHECK_MINUTES` and `SLACK_WEBHOOK_URL` from [Slack Webhooks](https://api.slack.com/incoming-webhooks) ) as shown in file [.env.example](/env.example)
+ This would assure that your keys are secured and index.js file is untouched.
+ - `npm start` (run the app, and like all the recent tweets (test) and send slack webhook notification)
+
+Deploying on Lambda
+===============
+
+ - `git clone https://github.com/alianjum0/tweet-like-bot
+ - `zip -r -D tweet-like-bot.zip node_modules index.js .env package.json`
+ - `aws lambda update-function-code --function-name tweet-like-bot --region eu-central-1 --zip-file fileb://tweet-like-bot.zip`
+ - `aws lambda invoke --function-name tweet-like-bot --region eu-central-1 out
+   --log-type Tail`
+
+
+ Thanks
+=================
+
+Inspired from https://github.com/gulzar1996/auto-like-my-gf-insta-pic.
--- index.js
+++ index.js
@@ -2,56 +2,95 @@
  * Required External Modules
  */
 
-import * as dotenv from "dotenv";
+import dotenv from 'dotenv'
 
-import {TwitterApi} from 'twitter-api-v2';
-import moment from 'moment';
+import { TwitterApi } from 'twitter-api-v2'
+import moment from 'moment'
+import { IncomingWebhook } from '@slack/webhook'
 
-dotenv.config();
+dotenv.config()
 
 /**
  * App Variables
  */
 
-if (!process.env.APP_KEY) {
-   process.exit(1);
+if (!process.env.CONSUMER_KEY) {
+  console.error('Set enviroment variables')
+  process.exit(1)
 }
 
+const userId = process.env.USER_ID
+const friendUserId = process.env.FRIEND_USER_ID
+
+// Get time minus minutes
+const checkMinutes = process.env.CHECK_MINUTES || 15
+const start_time = moment().subtract(checkMinutes, 'minutes').toISOString()
+const tweetOptions = {
+  exclude: 'retweets,replies',
+  start_time,
+  max_results: '100',
+}
+// Instanciate with desired auth type
+const consumerClient = new TwitterApi({
+  appKey: process.env.CONSUMER_KEY,
+  appSecret: process.env.CONSUMER_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_SECRET,
+})
+
+/**
+ * App Functions
+ */
+const getTweets = async (id) => {
+  const userTweets = await consumerClient.v2.userTimeline(id, tweetOptions)
+  return userTweets._realData.data || []
+}
+
+const likeTweet = async (tweet) => {
+  const { id } = tweet
+  await consumerClient.v2.like(userId, id)
+  console.log(`Liked tweet id: ${id}`)
+  return id
+}
+
+const sendSlackNotification = async (message) => {
+  const url = process.env.SLACK_WEBHOOK_URL
+
+  const webhook = new IncomingWebhook(url)
+
+  await webhook.send({
+    text: message,
+  })
+}
+
+/**
+ * Main Functions
+ */
 async function main() {
-  try {
-    // Instanciate with desired auth type (here's Bearer v2 auth)
-    //const twitterClient = new TwitterApi(process.env.BEARER_TOKEN);
-    const consumerClient = new TwitterApi({ 
-      appKey: process.env.APP_KEY, 
-      appSecret: process.env.APP_SECRET,
-      accessToken: process.env.ACCESS_TOKEN,
-      accessSecret: process.env.ACCESS_SECRET,
-    });
+  const userTweets = await getTweets(friendUserId)
 
-    const userId= process.env.USER_ID;
-    const selfUserId= process.env.SELF_USER_ID;
-
-    // Get tweets by userId of last day
-    const start_time = moment().subtract(1, "days").toISOString();
-    const tweetOptions= {
-      exclude: 'retweets,replies',
-      start_time,
-      max_results: '100'
-    };
-    const tweetsOfUser = await consumerClient.v2.userTimeline(userId, tweetOptions);
-    const data= tweetsOfUser._realData.data;
-
+  const count = userTweets.length
+  let message = ''
+  if (count) {
     //  Liking all tweets in parallel
-    var responses = await Promise.all(data.map(async (tweet) => {
-      const {id, text}= tweet;
-      await consumerClient.v2.like(selfUserId, tweet.id);
-      console.log(`Liked tweet id: ${id}, text: ${text}`);
-      return id;
-    }));
+    await Promise.all(userTweets.map(likeTweet))
 
-  } catch (e) {
-    console.error(JSON.stringify(e.message));
+    message = `${count} new tweet${count > 1 ? 's' : ''}.`
+    if (process.env.SLACK_WEBHOOK_URL) {
+      sendSlackNotification(message)
+    }
+  } else {
+    message = 'No new tweets.'
   }
+  console.log(message)
+  return message
 }
 
-main();
+export const handler = async (_event) => {
+  const message = await main()
+  const response = {
+    statusCode: 200,
+    body: message,
+  }
+  return response
+}
